57648c6943766346e1493a05d28569e8e437b77f
Luca Sforza <lucasforza1234@icloud.com>
2026-06-09 16:53:43 +0200

    Synthesis: qwen3-solidity-27B-Q6_K.gguf iteration 3 seed 13658655535873753781

--- src/Auction.sol
+++ src/Auction.sol
@@ -7,6 +7,10 @@ import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Recei
 
 contract Auction is IERC721Receiver {
     error AlreadyFinished();
+    error AlreadyBid();
+    error BidTooLow();
+    error AuctionNotFinished();
+    error NoBids();
 
     address public owner;
     NFT public auctionedToken;
@@ -14,10 +18,18 @@ contract Auction is IERC721Receiver {
     IERC721 public coll;
     uint256 public startTime;
     uint256 public endTime;
+    bool public finished;
+
+    address public highestBidder;
+    address public secondHighestBidder;
+    uint256 public secondHighestBid;
 
     struct NFT {
         uint256 tokenId;
     }
+
+    event BidPlaced(address indexed bidder, uint256 amount);
+    event AuctionFinished(address indexed winner, uint256 amount);
 
     constructor(
         IERC20 _token,
@@ -28,12 +40,32 @@ contract Auction is IERC721Receiver {
         uint256 _endTime
     ) {
         require(_collection.ownerOf(tokenId) == msg.sender && msg.sender == _owner && block.timestamp < _endTime);
+        owner = _owner;
+        auctionedToken = NFT(tokenId);
+        token = _token;
+        coll = _collection;
+        startTime = _startTime;
+        endTime = _endTime;
     }
 
     mapping(address => uint256) public bids;
     mapping(address => bool) public hasBid;
 
     function bid(uint256 amount) public {
+        if (hasBid[msg.sender]) revert AlreadyBid();
+        if (amount <= bids[highestBidder]) revert BidTooLow();
+
+        token.transferFrom(msg.sender, address(this), amount);
+
+        if (bids[highestBidder] > 0) {
+            secondHighestBidder = highestBidder;
+            secondHighestBid = bids[highestBidder];
+        }
+        highestBidder = msg.sender;
+        bids[msg.sender] = amount;
+        hasBid[msg.sender] = true;
+
+        emit BidPlaced(msg.sender, amount);
     }
 
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
@@ -41,5 +73,27 @@ contract Auction is IERC721Receiver {
     }
 
     function finishAuction() public returns (address) {
+        if (finished) revert AlreadyFinished();
+        if (block.timestamp < endTime) revert AuctionNotFinished();
+
+        finished = true;
+
+        if (highestBidder == address(0)) {
+            return address(0);
+        }
+
+        address winner = highestBidder;
+        uint256 winningBid = bids[winner];
+
+        coll.transferFrom(owner, winner, auctionedToken.tokenId);
+
+        if (secondHighestBidder != address(0)) {
+            token.transfer(secondHighestBidder, secondHighestBid);
+        }
+
+        token.transfer(owner, winningBid);
+
+        emit AuctionFinished(winner, winningBid);
+        return winner;
     }
 }
