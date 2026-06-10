e89d06384d85f0a325945dcbc7b1bba97a64c873
Luca Sforza <lucasforza1234@icloud.com>
2026-06-10 12:44:36 +0200

    Synthesis: qwen3.6-27B-Q6_K.gguf iteration 6 seed 13669265383690833024

--- src/Auction.sol
+++ src/Auction.sol
@@ -28,12 +28,30 @@ contract Auction is IERC721Receiver {
         uint256 _endTime
     ) {
         require(_collection.ownerOf(tokenId) == msg.sender && msg.sender == _owner && block.timestamp < _endTime);
+        token = _token;
+        coll = _collection;
+        owner = _owner;
+        startTime = _startTime;
+        endTime = _endTime;
+        auctionedToken = NFT({ tokenId: tokenId });
     }
+
+    bool private _finished;
 
     mapping(address => uint256) public bids;
     mapping(address => bool) public hasBid;
+    address[] private _bidderAddresses;
 
     function bid(uint256 amount) public {
+        if (_finished) revert AlreadyFinished();
+        require(amount > 0, "ZeroBid");
+        require(block.timestamp >= startTime, "NotStarted");
+        token.transferFrom(msg.sender, address(this), amount);
+        if (!hasBid[msg.sender]) {
+            _bidderAddresses.push(msg.sender);
+        }
+        bids[msg.sender] += amount;
+        hasBid[msg.sender] = true;
     }
 
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
@@ -41,5 +59,25 @@ contract Auction is IERC721Receiver {
     }
 
     function finishAuction() public returns (address) {
+        if (_finished) revert AlreadyFinished();
+        require(block.timestamp > endTime, "NotEnded");
+        _finished = true;
+
+        address winner;
+        uint256 highestAmount;
+
+        for (uint256 i = 0; i < _bidderAddresses.length; i++) {
+            address bidder = _bidderAddresses[i];
+            if (bids[bidder] > highestAmount) {
+                highestAmount = bids[bidder];
+                winner = bidder;
+            }
+        }
+
+        if (winner != address(0)) {
+            coll.safeTransferFrom(owner, winner, auctionedToken.tokenId);
+        }
+
+        return winner;
     }
 }
