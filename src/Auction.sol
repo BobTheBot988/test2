7e809d4c9751fd0ccc62c0402b4bcb3e2c8c6738
Luca Sforza <lucasforza1234@icloud.com>
2026-06-10 16:30:03 +0200

    Synthesis: api iteration 6 seed 5569179660433326947

--- src/Auction.sol
+++ src/Auction.sol
@@ -28,12 +28,33 @@ contract Auction is IERC721Receiver {
         uint256 _endTime
     ) {
         require(_collection.ownerOf(tokenId) == msg.sender && msg.sender == _owner && block.timestamp < _endTime);
+
+        token = _token;
+        coll = _collection;
+        auctionedToken = NFT(tokenId);
+        owner = _owner;
+        startTime = _startTime;
+        endTime = _endTime;
     }
 
     mapping(address => uint256) public bids;
     mapping(address => bool) public hasBid;
 
+    address public highestBidder;
+    uint256 public highestBid;
+    bool public finished;
+
     function bid(uint256 amount) public {
+        if (finished) revert AlreadyFinished();
+
+        token.transferFrom(msg.sender, address(this), amount);
+        bids[msg.sender] += amount;
+        hasBid[msg.sender] = true;
+
+        if (bids[msg.sender] > highestBid) {
+            highestBid = bids[msg.sender];
+            highestBidder = msg.sender;
+        }
     }
 
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
@@ -41,5 +62,13 @@ contract Auction is IERC721Receiver {
     }
 
     function finishAuction() public returns (address) {
+        if (finished) revert AlreadyFinished();
+
+        address winner = highestBidder;
+        finished = true;
+
+        coll.transferFrom(owner, winner, auctionedToken.tokenId);
+
+        return winner;
     }
 }
